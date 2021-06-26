a2991bdd7f1d9f8895d31b846d8003cf586b679a
dnlgrgly <dnlgrgly@gmail.com>
2021-06-26 16:39:23 +0200

    feat(mobile): update to restricted v3 api keys

--- apps/mobile/ios/Runner/AppDelegate.swift
+++ apps/mobile/ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyASK6nRQDQigpdU5k_WrD8EAZGDJ_daEX0")
+    GMSServices.provideAPIKey("AIzaSyCKEUk-fstmbjxDaBEqQRfXwIoCM-VzqB0")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
